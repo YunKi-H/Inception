499d2f5aa4f280b7f4d1212724bcdaca45843d4c
maemi <dbsrl759@naver.com>
2022-12-23 22:28:29 +0900

    containers on

--- Makefile
+++ Makefile
@@ -2,18 +2,21 @@ DOCKER_COMPOSE = docker compose
 DOCKER_COMPOSE_FILE = ./srcs/docker-compose.yml
 
 all:
-	mkdir -p /home/yuhwang/data/wordpress
-	mkdir -p /home/yuhwang/data/database
-	sudo $(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) up -d --build
+	mkdir -p ${HOME}/data/wordpress
+	mkdir -p ${HOME}/data/database
+	sudo $(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) up -d --build --force-recreate
 
 clean:
-	sudo $(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down
+	sudo $(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down -v --rmi all --remove-orphans
+
+fclean: clean
+	@sudo rm -rf ${HOME}/data
 	@sudo docker system prune -af
-	@sudo docker volume rm $$(docker volume ls -q)
-	@sudo docker network rm $$(docker network ls -q)
+	@sudo docker volume prune -f
+	@sudo docker network prune -f
 
 re:
-	$(MAKE) clean
+	$(MAKE) fclean
 	$(MAKE) all
 
-.PHONY: all clean re
+.PHONY: all clean fclean re
---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,69 @@
+# mkdir -p ${HOME}/data/database
+# mkdir -p ${HOME}/data/www
+
+# /usr/bin/mysql_install_db --user=${MYSQL_ROOT} \
+# --basedir=/usr \
+# --datadir=/var/lib/mysql
+
+# /usr/bin/mysqld --user=${MYSQL_ROOT} --datadir=/var/lib/mysql
+# sleep 2
+
+# # wordpress 데이터베이스 생성
+# mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;"
+# # 계정 생성
+# mysql -e "CREATE USER IF NOT EXISTS `$MYSQL_USER`@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+# # 데이터베이스 권한 부여
+# mysql -e "GRANT ALL PRIVILEGES ON `$MARIADB_NAME`.* TO `$MYSQL_USER`@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
+# # root에 비밀번호 생성
+# mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# # 새로고침
+# mysql -e "FLUSH PRIVILEGES;"
+
+
+# # pkill mysqld
+# mysqladmin -u${MYSQL_ROOT} -p${MYSQL_ROOT_PASSWORD} shutdown
+
+# # 재실행
+# /usr/bin/mysqld --user=${MYSQL_ROOT} --datadir=/var/lib/mysql
+
+
+# mkdir -p /home/yotak/data/db
+# mkdir -p /home/yotak/data/www
+sudo apt remove golang-docker-credential-helpers
+# --user myslqd(mysql 서버)를 구동하기 위한 사용자 이름
+# --basedir mysql설치 디렉토리 경로
+# --datadir mysql데이터 디렉토리 경로
+# https://pyrasis.com/book/DockerForTheReallyImpatient/Chapter16/02
+mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+
+# CREATE랑 GRANT사이에 들어가면 폭발....-- FLUSH PRIVILEGES;
+# GRANT ALL에서 IDENTIFIED BY $USER_PASSWORD
+cat > /tmp/mysql_init << EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+/usr/bin/mysqld --user=root --bootstrap < /tmp/mysql_init
+# rm -rf /tmp/sql
+
+# sleep 10
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+
+# mysql -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$USER_PASSWORD'";
+
+# mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER'@'%'";
+
+# mysql -e "ALTER USER root@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD'";
+
+# mysql -e "FLUSH PRIVILEGES";
+
+# mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+
+# while true; do { sleep 1 }; done;
